1fddb23e77012b5287f47413b051e113f0db9969
talha <talhaburakcuhadar@gmail.com>
2019-09-15 01:29:12 +0300

    bubble sort finished

--- src/App.js
+++ src/App.js
@@ -6,58 +6,66 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {highlighted: 0};
+    this.state = {
+      arr: [],
+      highlighted: 0
+    };
+    this.sortHistory = [];
+    this.highlightHistory = [];
+    this.sortHistoryTraverseIndex = 0;
+    this.interval = null;
+  }
+
+  componentWillMount() {
+    this.generateNewArray();
+  }
+
+  generateNewArray() {
+    if (this.interval) return;
+    this.sortHistoryTraverseIndex = 0;
+    this.sortHistory = [];
+    this.highlightHistory = [];
+
     let arr = [];
     for (let i = 0; i < 40; i++) {
       arr.push( Math.floor( Math.random() * 50) + 1);
     }
-    this.state.arr = arr;
-    this.sortHistory = [];
-    this.sortHistoryTraverseIndex = 0;
-    this.highlightHistory = [];
-  }
-
-  componentDidMount() {
-    //this.state.arr = this.arr;
-    /*
-    setTimeout(() => this.setState((state) => {
-      let temp = state.arr;
-      temp.push(12345);
-      return ({arr: temp});
-    }), 2000);
-     */
+    this.setState({arr: arr});
   }
 
   sort() {
+    if (this.interval) return;
     this.sortHistory = [];
     this.highlightHistory = [];
     console.log('SORT');
     let arr = this.state.arr;
     for (let i = 0; i < arr.length - 1; i++) {
-      let change = false;
       for (let j = 0; j < arr.length - (i + 1); j++) {
         if (arr[j] > arr[j + 1]) {
-          change = true;
           [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
           this.sortHistory.push(arr.slice());
           this.highlightHistory.push(j + 1);
         }
       }
-      if (!change) break;
     }
-    console.log(this.sortHistory);
     this.sortHistoryTraverseIndex = 0;
     if (this.sortHistory.length === 0) return;
-    let interval = setInterval( () => {
-      if (this.sortHistoryTraverseIndex >= this.sortHistory.length - 1) clearInterval(interval);
+    this.interval = setInterval( () => {
+      if (this.sortHistoryTraverseIndex >= this.sortHistory.length - 1) {
+        clearInterval(this.interval);
+        this.interval = null;
+      }
       this.setState({arr: this.sortHistory[this.sortHistoryTraverseIndex], highlighted: this.highlightHistory[this.sortHistoryTraverseIndex]});
       this.sortHistoryTraverseIndex++;
     }, 10);
   }
 
   handleSlide(event, newValue) {
-    console.log(newValue);
     if (newValue < 0 || newValue > this.sortHistory.length - 1) return;
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
     this.setState({arr: this.sortHistory[newValue], highlighted: this.highlightHistory[newValue]});
     this.sortHistoryTraverseIndex = newValue;
   }
@@ -66,20 +74,22 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Button onClick={ () => this.sort()} style={{backgroundColor: 'gray'}}>SORT</Button>
-        <div>
+        <div className="bar-wrapper">
           {this.state.arr.map((item, index) => <Bar key={index} size={item} highlighted={this.state.highlighted === index}/>)}
         </div>
-        <Slider className="slider" defaultValue={0} value={this.sortHistoryTraverseIndex} max={this.sortHistory.length} onChange={this.handleSlide.bind(this)} aria-labelledby="discrete-slider" />
+        <Slider className="slider" defaultValue={0} value={this.sortHistoryTraverseIndex} min={0} max={this.sortHistory.length + 1} onChange={this.handleSlide.bind(this)} aria-labelledby="discrete-slider" />
+        <div className="buttons-wrapper">
+          <Button className="button" onClick={ () => this.generateNewArray()}> NEW ARRAY </Button>
+          <Button className="button" onClick={ () => this.sort()} > SORT </Button>
+        </div>
       </div>
     );
   }
 }
 
 function Bar(props) {
-  console.log('proprs', props)
   return (
-    <div style={{display: 'inline-block', color: 'white', height: props.size * 10, width: 20, backgroundColor: props.highlighted ? 'red' : 'snow', marginRight: 3}}>
+    <div className="bar" style={{height: props.size * 10, backgroundColor: props.highlighted ? 'red' : 'snow'}}>
     </div>
   );
 }
